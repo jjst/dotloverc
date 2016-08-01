79754477e438fac4ea87efc3b633ae46d8b1b297
Wilhelmina Drengwitz <ghthor@gmail.com>
2016-08-01 02:09:55 -0400

    Add crowbar image and place in RC street

--- Scene.elm
+++ Scene.elm
@@ -206,9 +206,9 @@ rcStreet =
     , entities =
         [
             { kind = Item Crowbar
-            , hitbox = { x = 800, y = 880, width = 50, height = 100 }
+            , hitbox = { x = 636, y = 1080-79-32, width = 66, height = 32 }
             , description = "A well blacksmithed sturdy steel crowbar."
-            , imagePath = Nothing
+            , imagePath = Just "items/crowbar.png"
             }
 
             , planks
